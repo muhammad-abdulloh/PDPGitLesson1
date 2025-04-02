b3403f802fd19ccb1cf6de8ab8b4b0ff503b1dca
muhammad-abdulloh <clevercoderr@gmail.com>
2025-04-02 17:05:50 +0500

    CRUD Employee

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,27 +1,142 @@
-﻿using ConsoleApp3;
-using ConsoleApp3.Collectionss;
-using ConsoleApp3.InhertanceLesson;
+﻿
+using ConsoleApp3.Models;
+using ConsoleApp3.Services;
+using ConsoleApp3.Services.IServices;
 using Microsoft.Win32.SafeHandles;
 using System.Collections;
 using System.Data;
 
 class Program
 {
-    public static int Id { get; set; } = default;
-    public static string? Name { get; set; } = "";
-    public static string Name2 { get; set; } = "sdfsdf";
-    
     static void Main(string[] args)
     {
-        string ? name = null;
+        // create qilmoqchiman
+        EmployeeService employeeService = new EmployeeService();
 
-        if(string.IsNullOrEmpty(name))
+        while (true)
+        {
+            JumpToHigth:;
 
-        Console.WriteLine(Id);
-        Console.WriteLine(Name);
+            int choosenNumber = 0;
+
+            Console.WriteLine("Amalni tanlang: ");
+            Console.WriteLine("1: Create");
+            Console.WriteLine("2: Delete");
+            Console.WriteLine("3: Update");
+            Console.WriteLine("4: GetById");
+            Console.WriteLine("5: GetAll");
+
+            choosenNumber = int.Parse(Console.ReadLine());
+
+            if (choosenNumber == 1)
+            {
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Surname: ");
+                string surname = Console.ReadLine();
+
+                Console.Write("Position: ");
+                string position = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Yil: ");
+                int yil = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Oy: ");
+                int oy = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Kun: ");
+                int kun = int.Parse(Console.ReadLine());
 
 
+                Employee data = new Employee()
+                {
+                    Name = name,
+                    Surname = surname,
+                    Position = position,
+                    DateOfBirth = new DateTime(yil, oy, kun),
+                    Salary = salary
+                };
+
+
+                var result = employeeService.CreateEmployee(data);
+
+                Console.WriteLine(result);
+            }
+            else if (choosenNumber == 2)
+            {
+                var myId = Guid.Parse(Console.ReadLine());
+                var result = employeeService.DeleteEmployee(myId);
+
+                Console.WriteLine(result);
+            }
+            else if (choosenNumber == 3)
+            {
+                Console.WriteLine("Id ni kirgazing: ");
+                var employeeId = Guid.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Surname: ");
+                string surname = Console.ReadLine();
+
+                Console.Write("Position: ");
+                string position = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                Console.Write("Yil: ");
+                int yil = int.Parse(Console.ReadLine());
+
+                Console.Write("Oy: ");
+                int oy = int.Parse(Console.ReadLine());
+
+                Console.Write("Kun: ");
+                int kun = int.Parse(Console.ReadLine());
+
+
+                Employee data = new Employee()
+                {
+                    Id = employeeId,
+                    Name = name,
+                    Surname = surname,
+                    Position = position,
+                    DateOfBirth = new DateTime(yil, oy, kun),
+                    Salary = salary
+                };
+                var result = employeeService.UpdateEmployee(data);
+
+                Console.WriteLine($"Id:{result.Id} Name: {result.Name}, Surname: {result.Surname}, Position: {result.Position}, DateOfBirth: {result.DateOfBirth}, Salary: {result.Salary}");
+            }
+            else if (choosenNumber == 4)
+            {
+                Console.Write("Id ni kiriting: ");
+                var Id = Guid.Parse(Console.ReadLine());
+
+                var result = employeeService.GetByIdEmployee(Id);
+            }
+            else if (choosenNumber == 5)
+            {
+
+                IEnumerable<Employee> employees = employeeService.GetAllEmployes();
+
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine($"Id: {employee.Id} Name: {employee.Name}, Position: {employee.Position}, DateOfBirth: {employee.DateOfBirth}, Salary: {employee.Salary}");
+                }
+
+            }
+            else
+            {
+                goto JumpToHigth;
+            }
+            
+
+        }
     }
-
   
 }
---
+++ ConsoleApp3/Services/IServices/IEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Services.IServices
+{
+    interface IEmployeeService
+    {
+        public string CreateEmployee(Employee employee);
+        public Employee GetByIdEmployee(Guid Id);
+        public IEnumerable<Employee> GetAllEmployes();
+        public Employee UpdateEmployee(Employee employee);
+        public string DeleteEmployee(Guid Id);
+    }
+}
